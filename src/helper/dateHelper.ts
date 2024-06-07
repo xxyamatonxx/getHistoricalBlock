692bc9ba4a2707736cdc2f7e088d9aa350a78cd1
Yamato Ito <xxyamatonxx@gmail.com>
2024-06-07 19:41:07 +0900

    add avalancheX block fetcher

--- src/helper/dateHelper.ts
+++ src/helper/dateHelper.ts
@@ -2,10 +2,15 @@ export function unixToDate(unix: number): Date {
   return new Date(new Date(unix * 1000).toUTCString());
 }
 
-export function isCloseToMidnight(date: Date) {
+export function isCloseToMidnight(date: Date,limitMinute?:number) {
   const hours = date.getUTCHours();
   const minutes = date.getUTCMinutes();
   const seconds = date.getUTCSeconds();
 
-  return hours === 23 && minutes >= 57 && seconds >= 0;
+  if (!limitMinute && limitMinute !== 0) {
+    return hours === 23 && minutes >= 57 && seconds >= 0;
+  }
+
+
+  return hours === 23 && minutes >= limitMinute && seconds >= 0;
 }
